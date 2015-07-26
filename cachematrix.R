0a7b0cadb5e9668d1c5967034a130030d9870e1e
kalicharank <kalicharan.k@gmail.com>
2015-07-26 18:26:17 -0400

    Added Comments for function
    Added the comments for the first function

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,7 @@
 ## If the inverse has already been calculated (and the matrix has not changed), 
 ## then the cachesolve should retrieve the inverse from the cache.
 
-## Write a short comment describing this function
+# This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
 
